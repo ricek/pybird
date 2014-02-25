966b2bdc689cceff7879e7bb8da06f35865b0f2c
ricek <itsricek@gmail.com>
2014-02-24 21:51:04 -0500

    Updated game loop

--- pybird.py
+++ pybird.py
@@ -1,9 +1,5 @@
 from pybirdClass import *
 
-def gameOverMSG():
-    game.drawText("DEAD! Press ESC to quit",275,150,white)
-    game.over = True
-
 game = Game(700,500,"PyBird",30)
 bk = Image("img\\day.png",game)
 bar = Animation("img\\bar\\bar ",3,game,10)
@@ -17,23 +13,47 @@ for x in range(3):
     pipes.append(Pipe(game,(game.width/3*x),3))
     pipes[x].create()
 
+quit = False
 #Game Loop
-while not game.over:
+while not quit:
     game.processInput()
-    bk.draw()
-    for x in range(len(pipes)):
-        pipes[x].move()
-        if pipes[x].isOffScreen():
-            pipes[x].reset()
-        if bird.graphics.collidedWith(pipes[x].pipeTOP,"rectangular") or bird.graphics.collidedWith(pipes[x].pipeBOT,"rectangular"):
-            gameOverMSG()
-    bar.draw()
-   
-    if keys.Pressed[K_SPACE]:
-        bird.move()
-    else:
-        bird.fly()
+    
+    #Main Loop
+    while not game.over:
+        game.processInput()
+        bk.draw()
+        for x in range(len(pipes)):
+            pipes[x].move()
+            if pipes[x].isOffScreen():
+                pipes[x].reset()
+            if bird.graphics.collidedWith(pipes[x].pipeTOP,"rectangular") or \
+            bird.graphics.collidedWith(pipes[x].pipeBOT,"rectangular"):
+                game.over = True
+            if pipes[x].pipeTOP.right < bird.graphics.left:
+                game.score += 1
+                
+        bar.draw()
         
+        if bird.graphics.y > game.top and bird.graphics.y < bar.top:  
+            if keys.Pressed[K_SPACE]:
+                bird.move()
+            else:
+                bird.fly()
+        else:
+            game.over = True
+
+        game.displayScore()   
+        game.update(60)
+    #End Main Loop
+        
+    game.drawText("DEAD! ESC to quit / SPACE to continue",200,150,white)    
+    if keys.Pressed[K_ESCAPE]:
+        quit = True
+    if keys.Pressed[K_SPACE]:
+        game.over = False
+        bird.reset()
+                
     game.update(60)
-  
+#End Game Loop
+    
 game.quit()
